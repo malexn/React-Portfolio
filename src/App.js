03535c14729b1b7264252906f6df4891820d055c
malexn <alexander.nordh@gmail.com>
2019-11-14 22:06:14 +0100

    added about

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ function App() {
       <div className="App" id="colorlib-page">
         <Nav />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="/" exact component={About} />
           <Route path="/shop" exact component={Shop} />
           <Route path="/shop/:id" component={Item} />
         </Switch>
@@ -22,11 +21,5 @@ function App() {
     </Router>
   );
 }
-
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-);
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,35 @@
 import React from "react";
-import "../App.css";
+import profilepicture from "../images/portfolio.jpg";
 
 function About() {
   return (
-    <div>
-      <h1>About page</h1>
+    <div className="ftco-section about-section">
+      <div id="about" className="container">
+        <div className="row d-flex" data-scrollax-parent="true">
+          <div className="col-md-2"></div>
+          <div className="col-md-6 wrap ftco-animate">
+            <div className="about-desc">
+              <h1 className="bold-text" id="about">
+                About
+              </h1>
+              <div className="p-5">
+                <p>
+                  Master student, Interactive Media Technology | KTH Royal
+                  Institute of Technology, with an interest for front-end
+                  development, UI/UX, web accessibility and project management.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <img
+              src={profilepicture}
+              alt="profilepicture"
+              style={{ height: 300 + "px", width: 200 + "px" }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
