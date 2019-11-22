91fabc1722a34f3cf4fe150a87ad7c808b6edb71
malexn <alexander.nordh@gmail.com>
2019-11-22 16:46:38 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e7eb6b9f616f2d5f4ed12ad90fe76f4.js"
+  "/precache-manifest.4868284da1652551ae0fbe154028ce20.js"
 );
 
 self.addEventListener('message', (event) => {
