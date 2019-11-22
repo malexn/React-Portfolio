12d5391053761fe39fb4ebdbb9854b97b795af49
malexn <alexander.nordh@gmail.com>
2019-11-22 16:07:05 +0100

    remove service worker

--- src/index.js
+++ src/index.js
@@ -5,11 +5,5 @@ import "./css/style.css";
 import "./css/icomoon.css";
 import "../node_modules/video-react/dist/video-react.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
