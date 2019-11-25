963d5edf517bbda467fba1327683242d6841a43f
malexn <alexander.nordh@gmail.com>
2019-11-25 11:47:53 +0100

    changes

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -5,7 +5,7 @@ const Mobile = () => {
     <section className="mobile-container">
       <div className="content-mobile">
         <span className="justify-content-center">
-          Disclaimer: This site is optimized for mobile devices yet.
+          Disclaimer: This site is not optimized for mobile devices yet.
         </span>
       </div>
     </section>
